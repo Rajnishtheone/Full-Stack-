662eede7a205ecac7c3a4b37a81ca3425b1250b8
Rajnishtheone <Rajnishkk97@gmail.com>
2025-02-02 12:47:19 +0530

    JavaScript Basic Started Today

---
+++ Javascript/hello.js
@@ -0,0 +1,25 @@
+console.log("hello world");
+
+
+function printChai(){
+   console.log("Chai");
+   console.log("biscuit");
+}
+
+function bringbrush(numberofbrush){
+
+   console.log("brush le ke aoo bhai ");
+   console.log(`hani le aya ${numberofbrush} Brush`);
+
+}
+function add(a,b){
+   console.log(`the addition of ${a} and ${b} is :-  ${a+b}  `)
+}
+
+   printChai();
+
+   bringbrush(4);
+   add(1,1);
+   console.log(add(2,1));
+   
+
