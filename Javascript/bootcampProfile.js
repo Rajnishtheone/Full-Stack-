37e5815eb159eca9b481380a77d02108e4519837
Rajnishtheone <Rajnishkk97@gmail.com>
2025-02-03 19:22:02 +0530

    Added comments

--- Javascript/bootcampProfile.js
+++ Javascript/bootcampProfile.js
@@ -9,7 +9,7 @@ let temp1=undefined;
 
 console.log(temp1);
 
-Data types in javascript are of two types:- primitive and non-primitive
-primitive data types are:- string,number,boolean,null,undefined
- non-primitive data types are:- object,array,function
+// Data types in javascript are of two types:- primitive and non-primitive
+// primitive data types are:- string,number,boolean,null,undefined
+//  non-primitive data types are:- object,array,function
 
