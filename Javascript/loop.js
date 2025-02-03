25b571d0262bb00a6d46199d489e7af97a1cb4e8
Rajnishtheone <Rajnishkk97@gmail.com>
2025-02-03 20:30:38 +0530

    for loop ,  sum from  array

---
+++ Javascript/loop.js
@@ -0,0 +1,16 @@
+let array=["mango","banana","apple", "orange"];
+
+for(let i=0;i<array.length;i++){
+       console.log(array[i]);    
+}
+
+//sum of array elements
+
+let sum=[1,2,3,4,5,6,7,8,9,10];
+
+let total=0;
+for(let i=0;i<sum.length;i++){
+    total +=sum[i];
+
+}
+console.log(total);
